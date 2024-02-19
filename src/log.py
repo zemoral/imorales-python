d4bd303c99ad0fdd9b99de898a2240b76c7fd753
Isaiah Morales <isaiah.r.morales@gmail.com>
2024-02-18 20:57:45 -0600

    feat: added logging module

---
+++ src/log.py
@@ -0,0 +1,23 @@
+from logging import (
+    CRITICAL,
+    DEBUG,
+    ERROR,
+    INFO,
+    NOTSET,
+    WARN,
+    Formatter,
+    Logger,
+    getLogger,
+)
+
+GLOBAL_FORMAT = Formatter(
+    fmt="%()s :: %()s :: %()s :: %()s :: %()s :: %()s :: %()s",
+)
+GLOBAL_LOGGER = getLogger(
+    "GLOBAL"
+)
+
+
+class Trace(Logger):
+    def __init__(self, name: str, level: int = NOTSET) -> None:
+        pass
